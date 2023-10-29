f9650ef7c4645dff1bd580a938c6e4ee42e89bdf
CameronWood23 <cameron.wood2317@gmail.com>
2023-10-29 18:05:24 -0500

    Navigation Bar, some navigation fixes. Logo work still needs to be fixed.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-
 import HomeScreen from "../src/components/screens/homeScreen/HomeScreen";
+import NavigationBar from "./components/organisms/navigationBar/NavigationBar";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" index element={<HomeScreen />} />
+      <Route path="/" element={<NavigationBar />}>
+        <Route index element={<HomeScreen />} />
+      </Route>
     </Routes>
   );
 };
