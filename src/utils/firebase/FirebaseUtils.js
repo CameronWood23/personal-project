82bf3ae6d24739774430ad8e85b8dad52a25fe66
CameronWood23 <cameron.wood2317@gmail.com>
2023-10-30 18:21:26 -0500

    Firebase Util

---
+++ src/utils/firebase/FirebaseUtils.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzZgPfO6jUNM5CGt5bHIB0Z3iNkaQEjck",
+  authDomain: "bitbasket-ecommerce.firebaseapp.com",
+  projectId: "bitbasket-ecommerce",
+  storageBucket: "bitbasket-ecommerce.appspot.com",
+  messagingSenderId: "923950955859",
+  appId: "1:923950955859:web:ec041204e1d12fab3875a6",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
