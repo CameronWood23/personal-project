c42774568e990a2a991fe4b54ab2ebd6684f0aeb
CameronWood23 <cameron.wood2317@gmail.com>
2024-03-05 13:18:33 -0600

    Cart Icon / Dropdown / Styles / Navigation Bar addition

--- src/components/organisms/navigationBar/NavigationBar.jsx
+++ src/components/organisms/navigationBar/NavigationBar.jsx
@@ -1,13 +1,17 @@
 import { Outlet, Link } from "react-router-dom"
 import { Fragment, useContext } from "react"
 import { UserContext } from "../../../context/userContext"
+import { CartContext } from "../../../context/cartContext"
 import { signOutUser } from "../../../utils/firebase/FirebaseUtils"
 import { ReactComponent as Logo } from "../../../assets/BitbucketLogo.svg"
+import CartIcon from "../../atoms/cartIcon/CartIcon"
+import CartDropdown from "../../molecules/cartDropdown/CartDropdown"
 
 import "./navigation-bar.styles.scss"
 
 const NavigationBar = () => {
   const { currentUser } = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext)
 
   return (
     <Fragment>
@@ -31,7 +35,9 @@ const NavigationBar = () => {
                 SIGN IN
               </Link>
             )}
+            <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown />}
         </div>
       </div>
       <Outlet />
