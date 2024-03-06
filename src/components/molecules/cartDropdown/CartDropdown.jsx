0f9dd09749bf1c0626238a0f0c51ea06021fb3f5
CameronWood23 <cameron.wood2317@gmail.com>
2024-03-06 10:24:32 -0600

    Add To Cart Feature

--- src/components/molecules/cartDropdown/CartDropdown.jsx
+++ src/components/molecules/cartDropdown/CartDropdown.jsx
@@ -1,12 +1,27 @@
-import React from "react"
+import { useContext } from "react"
+import { CartContext } from "../../../context/cartContext"
 import Button from "../../atoms/button/Button"
+import CartItem from "../../molecules/cartItem/CartItem"
+
 import "./cart-dropdown.styles.scss"
 
-const CartDropdown = () => (
-  <div className="cart-dropdown-container">
-    <div className="cart-items" />
-    <Button>CHECKOUT</Button>
-  </div>
-)
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext)
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <Button>CHECKOUT</Button>
+    </div>
+  )
+}
 
 export default CartDropdown
