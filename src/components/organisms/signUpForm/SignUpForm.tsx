c7ef7927408afb7ecc0428425f5245367830a19e
CameronWood23 <cameron.wood2317@gmail.com>
2024-03-26 15:57:51 -0500

    Fix sign up form

--- src/components/organisms/signUpForm/SignUpForm.tsx
+++ src/components/organisms/signUpForm/SignUpForm.tsx
@@ -1,9 +1,12 @@
-import React, { ChangeEvent, FormEvent, useState } from "react"
+import React, { ChangeEvent, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAppDispatch } from "../../../redux/hooks"
+import { fetchCurrentUser } from "../../../redux/slices/userSlice"
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../../utils/firebase/FirebaseUtils"
-import { Button } from "../../atoms"
+import { DefaultButton } from "../../atoms"
 import { FormInput } from "../../molecules"
 import { SignUpContainer, TitleText } from "./signUpForm.styles"
 
@@ -25,12 +28,17 @@ const SignUpForm: React.FC = () => {
   const [formFields, setFormFields] = useState<FormFields>(defaultFormFields)
   const { displayName, email, password, confirmPassword } = formFields
 
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields)
   }
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const handleSubmit = async (event?: React.FormEvent<HTMLFormElement>) => {
+    if (event) {
+      event.preventDefault()
+    }
     if (password !== confirmPassword) {
       alert("Passwords do not match")
       return
@@ -43,6 +51,8 @@ const SignUpForm: React.FC = () => {
       if (userCredential && userCredential.user) {
         const { user } = userCredential
         await createUserDocumentFromAuth(user, { displayName })
+        dispatch(fetchCurrentUser())
+        navigate("/")
         resetFormFields()
       }
     } catch (error: any) {
@@ -62,7 +72,7 @@ const SignUpForm: React.FC = () => {
     <SignUpContainer>
       <TitleText>Don't have an Account?</TitleText>
       <span>Sign Up with your Email and Password</span>
-      <form onSubmit={handleSubmit}>
+      <form>
         <FormInput
           label="Display Name"
           type="text"
@@ -97,7 +107,13 @@ const SignUpForm: React.FC = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <Button>Sign Up</Button>
+        <DefaultButton
+          variant="contained"
+          buttonText="Sign Up"
+          size={"large"}
+          sx={{ backgroundColor: "black" }}
+          onClick={handleSubmit}
+        />
       </form>
     </SignUpContainer>
   )
